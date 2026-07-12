57e09d6eef2264e91daecfe992403f2c50a3bcdb
Brookelyn Meyer <bmeyer4s@semo.edu>
2026-07-11 20:24:21 -0500

    add total views calculation

--- creatorIQpythonpractice.py
+++ creatorIQpythonpractice.py
@@ -16,15 +16,35 @@
 
 # - loop through every video, print the title, print the platform, print the views, separate videos with a line.
 
+
+# the "videos" dictionary that holds all of the information about the videos
 videos = [
         
-        {"title": "Morning Routine", "Platform": "Tiktok", "Views": 100000, "likes": 10000, "comments": 200},
-        {"title": "Nespresso Latte", "Platform": "Instagram", "Views": 25000, "likes": 20000, "comments": 260}
+        {"title": "Morning Routine", "platform": "Tiktok", "views": 100000, "likes": 10000, "comments": 200},
+        {"title": "Nespresso Latte", "platform": "Instagram", "views": 25000, "likes": 20000, "comments": 260}
 ]
 
+# created a function that gets the total videos
+def get_video_views(videos):
+        total = 0
+
+        for video in videos:
+                total += video["views"]
+        
+        return total
+
+
+
+
+# loop that prints all of the data from the videos dictionary
 for video in videos:
         print("Title: ", video["title"])
-        print("Platform: ", video["Platform"])
-        print("Views: ", video["Views"])
+        print("platform: ", video["platform"])
+        print("views: ", video["views"])
         print("likes: ", video["likes"])
         print("comments: ", video["comments"])
+
+
+
+print("Total Views: ", get_video_views(videos))
+
