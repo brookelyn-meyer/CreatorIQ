16cd252ed1f7b8c085ef205e85defa9102802611
Brookelyn Meyer <bmeyer4s@semo.edu>
2026-07-11 21:25:07 -0500

    add video search feature

--- creatorIQpythonpractice.py
+++ creatorIQpythonpractice.py
@@ -33,8 +33,15 @@ def get_video_views(videos):
         
         return total
 
-
-
+def most_viewed(videos):
+        largest = None
+        for video in videos:
+                if largest is None:
+                        largest = video
+                elif video["views"] > largest["views"]:
+                        largest = video
+                
+                return largest
 
 # loop that prints all of the data from the videos dictionary
 for video in videos:
@@ -44,7 +51,30 @@ for video in videos:
         print("likes: ", video["likes"])
         print("comments: ", video["comments"])
 
+def print_video(video):
+    print("Title:", video["title"])
+    print("Platform:", video["platform"])
+    print("Views:", video["views"])
+    print("Likes:", video["likes"])
+    print("Comments:", video["comments"])
 
+def find_video(videos, title):
+        for video in videos:
+                if video["title"] == title:
+                        return video
+        
+        
+        return None
+
+
+most_viewed = most_viewed(videos)
+found_video = find_video(videos, "Morning Routine")
 
 print("Total Views: ", get_video_views(videos))
-
+print("Title: ", most_viewed["title"])
+print("Views: ", most_viewed["views"])
+print("Likes: ", most_viewed["likes"])
+print("Comments: ", most_viewed["comments"])
+print("The first video's information: ")
+print_video(videos[0])
+print(found_video)
